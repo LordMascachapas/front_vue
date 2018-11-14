586a3ada72a8b09c71dd05f33062bfd5ee8f4d35
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-11-14 16:36:35 +0100

    vue proyect template

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button>{{message}}</button>
+  </div>
+</template>
+
+<script>
+// <QuestionForm :question.sync='question'/>
+// import QuestionForm from '@/components/QuestionForm.vue';
+
+export default {
+  name: 'home',
+  components: {
+  },
+  data() {
+    return {
+      message: 'Un mensaje chachi',
+    };
+  },
+};
+</script>
+
+<style scoped lang="sass">
+body
+  background: #232d3d
+</style>
