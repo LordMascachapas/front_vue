a3a713e8ead84a84d870a866eafb1e2dfa4a1724
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-11-18 12:01:32 +0100

    form component send data to parent

--- src/components/SignForm.vue
+++ src/components/SignForm.vue
@@ -9,6 +9,7 @@
           <label for="InputUsername">Username</label>
           <input
             v-model="username"
+            name="username"
             type="text"
             class="form-control"
             id="InputUsername"
@@ -16,6 +17,7 @@
           <label for="InputPassword">Password </label>
           <input
             v-model="password"
+            name="password"
             type="password"
             class="form-control"
             id="InputPassword"
@@ -24,6 +26,7 @@
           <label for="InputConfirmPassword">Confirm password </label>
             <input
               v-model="passwordConfirm"
+              name="passwordConfirm"
               type="password"
               class="form-control"
               id="InputConfirmPassword"
@@ -31,7 +34,11 @@
           </div>
         </div>
       </div>
-      <button type="Submit" class="btn btn-success ribbon">Submit</button>
+      <button
+        type="button"
+        class="btn btn-success ribbon"
+        @click="sendData"
+      >Submit</button>
     </form>
     <button
       type="button"
@@ -44,6 +51,13 @@
 <script>
 export default {
   name: 'SignForm',
+  data() {
+    return {
+      registerModeString: 'SignUp',
+      passwordConfirm: '',
+      registerMode: false,
+    };
+  },
   props: {
     username: {
       type: String,
@@ -53,24 +67,26 @@ export default {
       type: String,
       default: '',
     },
-    passwordConfirm: {
-      type: String,
-      default: '',
-    },
-    registerMode: {
-      type: Boolean,
-      default: false,
-    },
-    registerModeString: {
-      type: String,
-      default: 'SignUp',
-    },
   },
   methods: {
     toggleRegisterMode() {
       this.registerMode = !this.registerMode;
       if (this.registerMode === false) this.registerModeString = 'SignUp';
       else this.registerModeString = 'SignIn';
+    },
+    sendData() {
+      if (this.registerMode === false) {
+        this.$emit('dataLogin', {
+          username: this.username,
+          pwd: this.password,
+        });
+      } else
+      if (this.passwordConfirm === this.password) {
+        this.$emit('dataRegist', {
+          username: this.username,
+          pwd: this.password,
+        });
+      } else alert('Error confirmando contrasena');
     },
   },
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,38 @@
 <template>
-  <div>
-    <button>{{message}}</button>
+  <div class="container">
+    <div class="col-xs-12 col-md8 col-md-offset-2">
+      <h1>{{username}}</h1>
+      <hr>
+      <SignForm
+        @dataLogin="submit"
+        @dataRegist="submit"/>
+    </div>
   </div>
 </template>
 
 <script>
 // <QuestionForm :question.sync='question'/>
-// import QuestionForm from '@/components/QuestionForm.vue';
+import SignForm from '@/components/SignForm.vue';
 
 export default {
-  name: 'home',
+  name: 'login',
   components: {
+    SignForm,
   },
   data() {
     return {
-      message: 'Un mensaje chachi',
+      username: '',
+      password: '',
     };
+  },
+  methods: {
+    submit(value) {
+      this.username = value.username;
+      this.password = value.pwd;
+    },
   },
 };
 </script>
 
-<style scoped lang="sass">
-body
-  background: #232d3d
+<style lang="sass">
 </style>
